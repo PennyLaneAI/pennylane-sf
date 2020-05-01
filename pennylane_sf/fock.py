b4d3b7575e75dba7a9c2d015074a1f23f05989c3
antalszava <antalszava@gmail.com>
2020-04-30 22:24:31 -0400

    Renaming to TensorN

--- pennylane_sf/fock.py
+++ pennylane_sf/fock.py
@@ -90,7 +90,7 @@ class StrawberryFieldsFock(StrawberryFieldsSimulator):
 
     _observable_map = {
         'NumberOperator': mean_photon,
-        'TensorNumberOperator': lambda state, wires, par: tuple((number_expectation(state, wires, par), None)),
+        'TensorN': lambda state, wires, par: tuple((number_expectation(state, wires, par), None)),
         'X': homodyne(0),
         'P': homodyne(np.pi/2),
         'QuadOperator': homodyne(),
