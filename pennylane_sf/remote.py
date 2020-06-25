d58620b56f83ae8bef4b9844fd5d916f2fc021c4
antalszava <antalszava@gmail.com>
2020-06-25 08:25:50 -0400

    Update pennylane_sf/remote.py
    Co-authored-by: Josh Izaac <josh146@gmail.com>

--- pennylane_sf/remote.py
+++ pennylane_sf/remote.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-Strawberry Fields Hardware device
-=================================
+Strawberry Fields Remote device
+===============================
 
 **Module name:** :mod:`pennylane_sf.remote`
 
