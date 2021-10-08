6d0258b71637c8c785e06a0a5c696bb5052eda86
Romain Moyard <rmoyard@gmail.com>
2021-10-08 13:40:55 -0400

    typo

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("pennylane_sf/_version.py") as f:
 
 requirements = [
     "strawberryfields>=0.15",
-    "git+git://github.com/PennyLaneAI/pennylane@master"
+    "pennylane @ git+https://github.com/PennyLaneAI/pennylane.git@master"
 ]
 
 info = {
