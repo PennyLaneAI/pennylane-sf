a3429a8d4e6018a21fcc3a79fb0d469b2574e85a
Jay Soni <jbsoni@uwaterloo.ca>
2021-11-02 15:18:43 -0400

    Release pl first assump in vbscript (#80)
    * updated logic to account for the assumption
    
    * small typo in path to version file
    
    * removed pl release flag
    
    * Update .github/workflows/vb_script.py
    
    Co-authored-by: antalszava <antalszava@gmail.com>
    
    * Added doc string in bump_version
    
    Co-authored-by: antalszava <antalszava@gmail.com>

--- .github/workflows/vb_script.py
+++ .github/workflows/vb_script.py
@@ -13,13 +13,25 @@
 # limitations under the License.
 import argparse
 import pennylane as qml
-pl_version = '"' + qml.version() + '"\n'  # we expect PL v0.X.0 here if we're releasing v0.X.0 of the plugin 
+pl_version = '"' + qml.version() + '"' # we expect PL v0.X.0 here if we're releasing v0.X+1.0 of the plugin
 
 
 def bump_version(version_line, pre_release):
     """ A helper function which takes the current version string and
     replaces it with the bumped version depending on the pre/post
     release flag.
+
+    Example:
+        Assume that we have PennyLane version 0.18.0 as the latest release on PyPi.
+
+        Pre-release:
+        We gather the version number for the latest of PennyLane release (i.e., v0.18.0) and increment the middle value
+        to obtain the version of the plugin that we'll release: v0.19.0.
+
+        Post-release:
+        We have just released the v0.19.0 version of the plugin and have v0.19.0 set as the version number.
+        We are entering a new development phase of the plugin attached to the next release, so we increment the
+        version number (0.20.0) and add the "-dev" suffix (0.20.0-dev).
 
     Args:
          version_line (string): The string containing the current
@@ -36,13 +48,15 @@ def bump_version(version_line, pre_release):
     curr_version = data[-1]
 
     if pre_release:
-        bumped_version = pl_version  # get current Pennylane version
-    else:
-        split_version = curr_version.split(".")  # "0.17.0" --> ["0,17,0"]
-        split_version[1] = str(int(split_version[1]) + 1)  # take middle value and cast as int and bump it by 1
-        split_version[2] = split_version[2].replace('"', '-dev"')  # add -dev, ["0,18,0"] --> ["0,18,0-dev"]
-        bumped_version = ".".join(split_version)
+        curr_version = pl_version  # get current Pennylane version
 
+    split_version = curr_version.split(".")  # "0.17.0" --> ["0,17,0"]
+    split_version[1] = str(int(split_version[1]) + 1)  # take middle value and cast as int and bump it by 1
+
+    if not pre_release:
+        split_version[2] = split_version[2].replace('"', '-dev"')  # add -dev, ["0,18,0"] --> ["0,18,0-dev"]
+
+    bumped_version = ".".join(split_version)
     data[-1] = bumped_version
     return " ".join(data), bumped_version
 
