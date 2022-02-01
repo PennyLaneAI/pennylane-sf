a384d8d0427872448519b2c037aa9a58b27b6564
Samuel Banning <samcbanning@gmail.com>
2022-02-01 12:34:10 -0500

    fixed

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("pennylane_sf/_version.py") as f:
 
 requirements = [
     "strawberryfields>=0.20",
-    "pennylane @ https://github.com/PennyLaneAI/pennylane"
+    "pennylane @ https://github.com/PennyLaneAI/pennylane.git"
 ]
 
 info = {
