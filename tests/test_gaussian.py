fixed tests requiring gradients

tests/test_gaussian.py
@@ -622,8 +622,8 @@ class TestVariance:
             qml.Displacement(a, 0, wires=0)
             return qml.var(qml.NumberOperator(0))
 
-        n = 0.12
-        a = 0.765
+        n = np.array(0.12, requires_grad=True)
+        a = np.array(0.765, requires_grad=True)
 
         var = circuit(n, a)
         expected = n ** 2 + n + np.abs(a) ** 2 * (1 + 2 * n)
@@ -830,8 +830,8 @@ class TestProbability:
             qml.Displacement(a, phi, wires=0)
             return qml.probs(wires=[0])
 
-        a = 0.4
-        phi = -0.12
+        a = np.array(0.4, requires_grad=True)
+        phi = np.array(-0.12, requires_grad=True)
 
         # construct tape
         circuit.construct([a, phi], {})
@@ -856,8 +856,8 @@ class TestProbability:
             qml.Displacement(a, 0, wires=0)
             return qml.var(op)
 
-        n = 0.12
-        a = 0.765
+        n = np.array(0.12, requires_grad=True)
+        a = np.array(0.765,requires_grad=True)
 
         var = circuit(n, a)
         expected = n ** 2 + n + np.abs(a) ** 2 * (1 + 2 * n)